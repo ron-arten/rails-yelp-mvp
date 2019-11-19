2c6bd2e518a48594771139d4743fbde95205633c
Ron Artenstein <ronartenstein@gmail.com>
2019-11-19 17:08:30 +0200

    rails new with some new gems, BS and SimpleForm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian'
+  },
+  {
+    name:         'La Fromage',
+    address:      '50B Blyat High Rd, Moscow E1 7PQ',
+    category:  'french'
+  },
+  {
+    name:         'Moshi Moshi',
+    address:      '75 Okinawa Rd, Tokyo',
+    category:  'japanese'
+  },
+  {
+    name:         'Magma',
+    address:      '17 Lilen St, Belgium',
+    category:  'belgian'
+  },
+  {
+    name:         'Pizza Pasta',
+    address:      '20 Mama Mia St, Rome',
+    category:  'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
